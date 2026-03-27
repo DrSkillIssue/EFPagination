91ccadc6dd2a3e4c9556c14d8fa5af754fa9aa5a
DrSkillIssue <drskillissue@zhl.dev>
2026-03-27 00:43:14 -0500

    feat: add executor overload for column values

--- test/EFPagination.Tests/PaginationExecutorIntegrationTests.cs
+++ test/EFPagination.Tests/PaginationExecutorIntegrationTests.cs
@@ -45,6 +45,40 @@ public class PaginationExecutorIntegrationTests
     }
 
     [Fact]
+    public async Task ExecuteAsync_AfterColumnValues_UsesDirectValuePath_AndSkipsCount()
+    {
+        var definition = PaginationQuery.Build<MainModel>(b => b.Ascending(x => x.Created).Ascending(x => x.Id));
+        var reference = await _dbContext.MainModels.OrderBy(x => x.Created).ThenBy(x => x.Id).Skip(9).FirstAsync();
+        ColumnValue[] referenceValues =
+        [
+            new(nameof(MainModel.Created), reference.Created),
+            new(nameof(MainModel.Id), reference.Id),
+        ];
+
+        var page = await PaginationExecutor.ExecuteAsync(_dbContext.MainModels, definition, 10, includeCount: false, referenceValues);
+
+        page.Items.Select(x => x.Id).Should().BeEquivalentTo(Enumerable.Range(11, 10), options => options.WithStrictOrdering());
+        page.HasMore.Should().BeTrue();
+        page.TotalCount.Should().Be(-1);
+    }
+
+    [Fact]
+    public async Task ExecuteAsync_WithColumnValues_RejectsComputedDefinitions()
+    {
+        var definition = PaginationQuery.Build<MainModel>(b => b.Ascending(x => x.CreatedNullable ?? DateTime.MinValue).Ascending(x => x.Id));
+        ColumnValue[] referenceValues =
+        [
+            new(nameof(MainModel.CreatedNullable), DateTime.MinValue),
+            new(nameof(MainModel.Id), 10),
+        ];
+
+        var act = () => PaginationExecutor.ExecuteAsync(_dbContext.MainModels, definition, 10, includeCount: false, referenceValues);
+
+        await act.Should().ThrowAsync<InvalidOperationException>()
+            .WithMessage("Direct column-value pagination only supports member-access columns*");
+    }
+
+    [Fact]
     public async Task ExecuteAsync_LastPageOfFilteredQuery_ReturnsNoMore_AndFilteredCount()
     {
         var definition = PaginationQuery.Build<MainModel>(b => b.Ascending(x => x.Id));
